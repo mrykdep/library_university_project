e35428575e9d91ec67523a2309157a505d3d0c4a
Myrkdep <mardep@LAPTOP-TN4E0328.localdomain>
2022-08-10 16:05:13 +0430

    Added http auth using flask_http auth. admin user in now configured in first startup. fixed some little bugs and a little refactoring.

--- app.py
+++ app.py
@@ -3,16 +3,19 @@ from datetime import date , timedelta
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_httpauth import HTTPBasicAuth
 import os
 
+#IMPORTANT MUST READ: database should be created manually
 #config values
 DEFAULT_BORROW_DAYS = 30
 DEFAULT_MEMBERSHIP_YEARS = 1
 DATABASE_USERNAME = 'pedram'
 DATABASE_PASSWORD = 'Project.4003'
-#database should be created manually
 DATABASE_NAME = 'library'
 CURRENT_PREFIX = '401'
+ADMIN_NAME = 'Pedram Akbari'
+ADMIN_PASSWORD = 'Project.4003'
 
 app = Flask(__name__)
 url = f'mysql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@localhost/{DATABASE_NAME}'
@@ -25,6 +28,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 #init ma
 ma = Marshmallow(app)
+#init auth
+auth = HTTPBasicAuth()
 
 #classes
 class TempID(db.Model):
@@ -37,17 +42,35 @@ class Member(db.Model):
     member_id = db.Column(db.BigInteger, primary_key=True)
     member_name = db.Column(db.String(200), nullable=False)
     member_password = db.Column(db.String(64), nullable=False)
-    member_type = db.Column(db.SmallInteger, nullable=False)
+    member_type = db.Column(db.String(8), nullable=False)
     member_expire_date = db.Column(db.Date, nullable=False)
 
-    def __init__(self, member_name, member_password, member_type):
+    def __init__(self, member_name, member_password):
         current_date = date.today()
         current_id = TempID.query.get(1)
         self.member_id = current_id.next_id
         self.member_name = member_name
         self.member_password = hashlib.sha256(member_password.encode('ascii')).hexdigest()
-        self.member_type = member_type
+        self.member_type = 'user'
         self.member_expire_date = current_date.replace(year = current_date.year + DEFAULT_MEMBERSHIP_YEARS)
+
+    def is_valid(member_id,member_password):
+        if Member.query.get(member_id).member_password == member_password:
+            return True
+        else:
+            return False
+
+    def get_role(member_id):
+        return Member.query.get(member_id).member_type
+    
+    def increment_next_id():
+        current_date = date.today()
+        current_id = TempID.query.get(1)
+        if(str(current_id.next_id)[0:2] == str(current_date.year)[2:]):
+            current_id.next_id += 1
+        else:
+            current_id.next_id = int(str(date.today().year)[2:] + '0000000')
+        db.session.commit()
 
 class Author(db.Model):
     author_id = db.Column(db.Integer, primary_key=True)
@@ -170,27 +193,70 @@ if(not os.path.isfile('configed')):
     tempid_config = TempID()
     db.session.add(tempid_config)
     db.session.commit()
+    #add admin user
+    current_date = date.today()
+    member_name = ADMIN_NAME
+    member_password = ADMIN_PASSWORD
+    member_type = 'admin'
+    new_member = Member(member_name, member_password, member_type)
+    db.session.add(new_member)
+    #config next_id for next user creation
+    Member.increment_next_id()
+    db.session.commit()
     fp = open('configed', 'x')
     fp.close()
 
+
+@auth.verify_password
+def verify_password(member_id, hashed_member_password):
+    if Member.is_valid(member_id, hashed_member_password):
+        return member_id
+    else:
+        return None
+
+@auth.get_user_roles
+def get_user_roles(member_id):
+    return Member.get_role(member_id)
+    
+
 #routes
-@app.route('/api/member', methods=['POST'])
+@app.route('/api/signup_admin', methods=['POST'])
+@auth.login_required(role='admin')
 def add_member():
     current_date = date.today()
     member_name = request.json['member_name']
     member_password = request.json['member_password']
     member_type = request.json['member_type']
-    new_member = Member(member_name, member_password, member_type)
+    new_member = Member(member_name, member_password)
+    new_member.member_type = member_type
     db.session.add(new_member)
-    current_id = TempID.query.get(1)
     #config next_id for next user creation
-    if(str(current_id.next_id)[0:2] == str(current_date.year)[2:]):
-          current_id.next_id += 1
-    else:
-        current_id.next_id = int(str(date.today().year)[2:] + '0000000')
+    Member.increment_next_id()
     db.session.commit()
     return jsonify({'status': 'ok'})
 
+@app.route('/api/signup', methods=['POST'])
+@auth.login_required(role=['admin', 'operator'])
+def add_member():
+    current_date = date.today()
+    member_name = request.json['member_name']
+    member_password = request.json['member_password']
+    new_member = Member(member_name, member_password)
+    db.session.add(new_member)
+    #config next_id for next user creation
+    Member.increment_next_id()
+    db.session.commit()
+    return jsonify({'status': 'ok'})
+
+#err need to implent a way to ban users after some amount of wrong login
+@app.route('/api/login', methods=['POST'])
+def login():
+    member_id = request.json['member_id']
+    member_password = request.json['member_password']
+    if verify_password(member_id, member_password):
+        return jsonify({'status': 'ok'})
+    else:
+        return jsonify({'status': 'not ok'})
 
 if __name__ == '__main__':
     app.run(debug=True)
